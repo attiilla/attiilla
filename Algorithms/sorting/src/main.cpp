eb41944c2d2f33ccb461d3984834fdc7820dd5ba
attiilla <atilappl@gmail.com>
2022-09-23 20:45:54 +0100

    renamed random to shuffler and implemented methods

--- Algorithms/sorting/src/main.cpp
+++ Algorithms/sorting/src/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-//#include "random.h"
+#include "shuffler.h"
+#include <time.h>
 //#include "sort.h"
 //file used to learn about makefile
+
+void print(std::vector<int> v){
+    int n = v.size();
+    for(int i = 0;i<n;i++){
+        std::cout<<v[i]<<std::endl;
+    }
+}
+
 int main(){
-    std::cout<<"eita"<<std::endl;
+    
     return 0;
 }
 
